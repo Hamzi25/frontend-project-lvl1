11e0f8d15873986b67a1061c82f23c1aac3a4163
hamza torshkho <hamza.torshkho@gmail.com>
2022-03-27 18:22:50 +0300

    Update

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line import/no-relative-packages
 import readlineSync from '../readline-sync/lib/readline-sync.js';
 
-const userName = () => { 
-    const name = readlineSync.question('May I have your name? '); 
-    console.log(`Hello, ${name}!`); 
-}; 
+const userName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
 export default userName;
