8e1dcbc9a49ee0287ec5bbd1b63e4c701407b8bd
hamza torshkho <hamza.torshkho@gmail.com>
2022-04-15 13:51:17 +0300

    br

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import brainCalc from '../brain-games/calc.js';
+
+brainCalc();
---
+++ brain-games/calc.js
@@ -0,0 +1,33 @@
+import playTheGame from '../src/index.js';
+import getRandomNumber from '../src/getRandomNum.js';
+
+const calcCondition = 'What is the result of the expression?';
+
+const getTypeOfCulc = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      return 'Sorry, this operand is not allowed.';
+  }
+};
+
+const getRundomOper = (oper) => oper[getRandomNumber(0, oper.length - 1)];
+const operators = ['+', '-', '*'];
+
+const newRound = () => {
+  const num1 = getRandomNumber();
+  const num2 = getRandomNumber();
+  const randomOper = getRundomOper(operators);
+  const answer = String(getTypeOfCulc(num1, num2, randomOper));
+  const question = `${num1} ${randomOper} ${num2}`;
+  return [question, answer];
+};
+
+const brainCalc = () => playTheGame(calcCondition, newRound);
+
+export default brainCalc;
